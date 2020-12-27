4182f9c693b54fde178ca8bc1419e3ce6eb0b3d2
Lawrence Nicastro <lawrence.nicastro1@gmail.com>
2020-12-27 18:02:30 -0500

    add simple form

---
+++ src/components/InputField.tsx
@@ -0,0 +1,35 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { useField } from "formik";
+import React from "react";
+import { InputHTMLAttributes } from "react";
+
+// input field component takes in any props that a regular input component will take
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  label: string;
+  name: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _,
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input
+        {...field}
+        {...props}
+        id={field.name}
+        placeholder={props.placeholder}
+      />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
